50fcd2e5a6c6cb506bbe2ef2020d463d8b6da6c0
Yinka <akinn.yinka@gmail.com>
2021-07-28 21:09:01 +0100

    more styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import moreOf from "./images/moreoff.png";
 import onBulb from "./images/on-bulb.png";
@@ -21,21 +20,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Turn on the light by clicking the switch</h1>
+        <h1>Turn {isOn?<span>off</span>:<span>on</span>} the light by clicking the switch</h1>
         <img className ="bulbs" src={isOn ? onBulb : moreOf} alt ="off bulb"/>
         <button className="bulb-switch" onClick={bulbSwitch}>Switch</button>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Tari.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
